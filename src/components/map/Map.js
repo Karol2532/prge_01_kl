17509ba2bdb2252c0f0dc7ccde5be5a6ad657ecc
Karol2532 <149382213+Karol2532@users.noreply.github.com>
2024-06-18 16:15:13 +0200

    added map funcions - workers and workplaces

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -11,14 +11,15 @@ import "leaflet/dist/leaflet.css";
 import axios from "axios";
 
 function Map() {
-  const [wojewodztwa, setWojewodztwa] = useState(null);
+  const [workers, setWorkers] = useState(null);
 
   const makePopup = (feature, layer) => {
     if (feature.properties) {
       layer.bindPopup(`
-      <h1>Dane województwa</h1>
-      <strong>Nazwa:</strong> ${feature.properties.JPT_NAZWA_}<br>
-      <strong>Powierzchnia:</strong> ${feature.properties.JPT_POWIER}
+      <h1>Dane pracownika</h1>
+      <strong>Imię:</strong> ${feature.properties.name}<br>
+      <strong>Nazwisko:</strong> ${feature.properties.lastname}<br>
+      <strong>Miasto:</strong> ${feature.properties.city}
       `);
     }
   };
@@ -27,9 +28,36 @@ function Map() {
     const getData = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Apoligony&maxFeatures=50&outputFormat=application%2Fjson"
+          "http://localhost:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Aworkers&maxFeatures=50&outputFormat=application%2Fjson"
         );
-        setWojewodztwa(response.data);
+        setWorkers(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    getData();
+  }, []);
+
+  const [workplaces, setWorkplaces] = useState(null);
+
+  const makePopup2 = (feature, layer) => {
+    if (feature.properties) {
+      layer.bindPopup(`
+      <h1>Dane oddziału</h1>
+      <strong>Nazwa:</strong> ${feature.properties.name}<br>
+      <strong>Miasto:</strong> ${feature.properties.city}<br>
+      <strong>Kraj:</strong> ${feature.properties.country}
+      `);
+    }
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/geoserver/prge/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=prge%3Aworkplaces&maxFeatures=50&outputFormat=application%2Fjson"
+        );
+        setWorkplaces(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -39,7 +67,7 @@ function Map() {
 
   return (
     <div className="map">
-      <MapContainer center={[52.2322222, 21.0]} zoom={10}>
+      <MapContainer center={[52.27, 19.24]} zoom={6}>
         <LayersControl>
           <LayersControl.BaseLayer name="OSM">
             <TileLayer url="https://tile.openstreetmap.org/{z}/{x}/{y}.png" />
@@ -50,15 +78,16 @@ function Map() {
           <LayersControl.BaseLayer name="Google Satelite">
             <TileLayer url="http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}" />
           </LayersControl.BaseLayer>
-          <LayersControl.BaseLayer name="Granice województw DB">
-            <WMSTileLayer
-              layers="granice_wojewodztw"
-              url="http://127.0.0.1:8080/geoserver/prge/wms"
-            />
-          </LayersControl.BaseLayer>
-          <LayersControl.Overlay checked name="Granice województw DB WFS">
-            {wojewodztwa ? (
-              <GeoJSON data={wojewodztwa} onEachFeature={makePopup} />
+          <LayersControl.Overlay checked name="Pracownicy firmy">
+            {workers ? (
+              <GeoJSON data={workers} onEachFeature={makePopup} />
+            ) : (
+              ""
+            )}
+          </LayersControl.Overlay>
+          <LayersControl.Overlay checked name="Oddziały firmy">
+            {workplaces ? (
+              <GeoJSON data={workplaces} onEachFeature={makePopup2} />
             ) : (
               ""
             )}
